5ca53440eda18eb0036ad69497d06936b99aa029
Gioel Amalfitano <gioel.amalfitano@students.fhnw.ch>
2019-09-21 20:50:39 +0200

    fix

--- Computergrafik/src/Colors.java
+++ Computergrafik/src/Colors.java
@@ -1,13 +1,8 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
 import javafx.scene.image.PixelWriter;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
